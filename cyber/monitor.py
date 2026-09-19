3df878dc957ba2862cd96afc81bbb05978294808
Pasula Reshma <reshmapasula9@gmail.com>
2026-09-19 20:36:02 +0530

    Add cyber monitoring module

---
+++ cyber/monitor.py
@@ -0,0 +1,9 @@
+def get_cyber_status():
+    return {
+        "status": "MONITORING",
+        "threat_level": "LOW",
+        "threats_detected": 0,
+        "blocked": 24,
+        "network": "MONITORING",
+        "endpoint": "SECURE"
+    }
